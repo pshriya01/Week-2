751ab0260b48e84a520b103be6b86e8dbb0cac9e
pshriya01 <shriya.shriya16@gmail.com>
2025-05-31 21:14:05 +0530

    execution context

---
+++ ExecutionContext.js
@@ -0,0 +1,15 @@
+var age = 25;
+
+function displayAge() {
+  console.log("The age is: ", age);
+}
+
+function changeAge() {
+  age = 30;
+  console.log("The updated age is: ", age);
+}
+
+displayAge();
+changeAge();
+
+displayAge();
