3fc8e8e40dae8b6979bf2896b440035303e504f0
pshriya01 <shriya.shriya16@gmail.com>
2025-05-31 23:47:29 +0530

    done

---
+++ promises.js
@@ -0,0 +1,9 @@
+function timer(duration, onComplete) {
+  setTimeout(() => {
+    onComplete(`Timer of ${duration} ms finished`);
+  }, duration);
+}
+
+timer(3000, (message) => {
+  console.log(message);
+});
