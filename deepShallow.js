751ff894aca6c9aa7a5e0134c13eaf6837bbc263
pshriya01 <shriya.shriya16@gmail.com>
2025-05-31 22:52:50 +0530

    done

---
+++ deepShallow.js
@@ -0,0 +1,11 @@
+function deepClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+const original = { name: "Alice", hobbies: ["reading", "traveling"] };
+const clone = deepClone(original);
+
+clone.hobbies.push("coding");
+
+console.log("Original:", original);
+console.log("Clone:", clone);
